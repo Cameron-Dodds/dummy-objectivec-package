c6ece1a809cfe2b0b8a064b212c66a8070218f63
Cameron <cameron.dodds@imperva.com>
2022-11-22 12:31:48 +0000

    Updated the names of things to be consistent

--- Package.swift
+++ Package.swift
@@ -3,13 +3,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "dummy-swift-package",
+    name: "dummy-objectivec-package",
     products: [],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "dummy-objectivec-framework",
+            name: "dummy-objectivec-package",
             url: "https://d26w1q8rapqjdm.cloudfront.net/dummy-objectivec-package.xcframework.zip",
-	    checksum: "db5b954a6db6dad85003d181ba0e9e841f679fb6bc0ceeda8747f8a23736e39f"),
+	    checksum: "17b574bad64f10e0eef2891bfac0495919f682b8a3bc8677edee336fe6e622c8"),
     ]
 )
